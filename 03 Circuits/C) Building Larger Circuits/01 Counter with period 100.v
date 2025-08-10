7d78821f513d0f4fcf987bbc58e9b3242719019f
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2025-08-10 19:33:38 +0530

    Create 01 Counter with period 100.v

---
+++ 03 Circuits/C) Building Larger Circuits/01 Counter with period 100.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input reset,
+    output reg[9:0] q);
+    always @(posedge clk)begin
+        if(reset || q==10'd999)
+            q<=9'b0;
+        else
+            q<=q+1;
+    end
+endmodule
