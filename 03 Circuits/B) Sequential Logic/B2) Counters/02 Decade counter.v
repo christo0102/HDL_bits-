8ca116ac1a898c226e649cd460c6e7157df8d941
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2025-08-10 16:36:06 +0530

    Create 02 Decade counter.v

---
+++ 03 Circuits/B) Sequential Logic/B2) Counters/02 Decade counter.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+    always @(posedge clk)begin
+        if(reset)
+            q<=4'b0;
+        else
+            q<=(q==4'b1001)?4'b0:q+1;
+            end
+endmodule
