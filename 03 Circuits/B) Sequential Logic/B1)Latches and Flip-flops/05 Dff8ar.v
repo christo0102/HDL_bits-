621ebd95d262ef8fc3978b1ecbc22aa852520e26
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2025-08-10 16:19:40 +0530

    Create 05 Dff8ar.v

---
+++ 03 Circuits/B) Sequential Logic/B1)Latches and Flip-flops/05 Dff8ar.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always @(posedge clk , posedge areset)begin
+        if(areset)
+            q<=1'b0;
+        else
+            q[7:0]<=d[7:0];
+    end
+endmodule
